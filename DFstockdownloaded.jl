ebf7ea7804df6e1ba3d43f7925e8196290e73b87
milktrader <milktrader@gmail.com>
2012-12-04 11:42:30 -0500

    fixed DataFrame inside function

--- DFstockdownloaded.jl
+++ DFstockdownloaded.jl
@@ -5,12 +5,12 @@ require("DataFrames")
 df = DataFrame(quote
 
 # Date     = sa[:,1]
- Open     = float64(sa[:,2])
- High     = float64(sa[:,3])
- Low      = float64(sa[:,4])
- Close    = float64(sa[:,5])
- Volume   = int64(sa[:,6])
- Adjusted = float64(sa[:,7])
+ Open     = float64($(sa[:,2]))
+ High     = float64($(sa[:,3]))
+ Low      = float64($(sa[:,4]))
+ Close    = float64($(sa[:,5]))
+ Volume   = int64($(sa[:,6]))
+ Adjusted = float64($(sa[:,7]))
 end);
 
 end
