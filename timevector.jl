83c6f7635af8e1772c24049ac684eaf10bb00159
milktrader <milktrader@gmail.com>
2012-12-04 16:24:08 -0500

    refactored timevector a bit

--- timevector.jl
+++ timevector.jl
@@ -1,19 +1,15 @@
 ## sa is a string array
-## ctseed is a Calendar array (very kludgy, I know)
-## ct_type is an instance of the Calendar type
 ## ct_vec is filled with elements of type Calendar, otherwise
 ## there is much agony, pain and a general state of frustration
 
 ## wish list: refactor to pass only sa, return ct_vec and
 ## clean up the middle, ugly parts
 
-function timevector(sa, ctseed)
+function timevector(sa)
 
-require("Calendar")
+load("~/git/Calendar.jl/src/Calendar.jl") 
 
-ct_type = ctseed[1]
-
-ct_vec = fill(ct_type, length(sa))
+ct_vec = fill(now(), length(sa))
 
 for i in 1:length(sa)
   ct_vec[i] = parse("yyyy-MM-dd", sa[i])
