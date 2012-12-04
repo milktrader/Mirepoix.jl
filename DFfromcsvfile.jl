6f60a633a97301d9d0b47de028fa52b65bd8e3f4
milktrader <milktrader@gmail.com>
2012-12-04 12:07:12 -0500

    renamed DF functions to verbose and descriptive names

--- DFfromcsvfile.jl
+++ DFfromcsvfile.jl
@@ -2,20 +2,18 @@ function DFstock(s)
 
 require("DataFrames")
 
-  function foo(s)
     sym = csvread(s)
     sym = sym[2:end, 2:7] 
     sym = flipud(sym)
     sym = float64(sym)
-  end
 
   stock = DataFrame(quote
-    Op  = sym[:,1]
-    Hi  = sym[:,2]
-    Lo  = sym[:,3]
-    Cl  = sym[:,4]
-    Vol = sym[:,5]
-    Adj = sym[:,6]
+    Op  = $(sym[:,1]) 
+    Hi  = $(sym[:,2])
+    Lo  = $(sym[:,3])
+    Cl  = $(sym[:,4])
+    Vol = $(sym[:,5])
+    Adj = $(sym[:,6])
   end);
 end
 
