94d4703dd438b41b426a95d8b08bdce4c73f28bf
milktrader <milktrader@gmail.com>
2012-12-21 11:26:24 -0500

    adjusted convert args in read_time

--- read_time.jl
+++ read_time.jl
@@ -10,7 +10,7 @@ time_based_df = read_table(x);
 
 # foo = convert(Array{UTF16String}, time_based_df[:,1])
 
- bar = map(x -> parse("yyyy-MM-dd", x), convert(Array{UTF16String}, time_based_df[:,1]))
+ bar = map(x -> parse("yyyy-MM-dd", x), convert(Array{UTF16String}, vector(time_based_df[:,1])))
 
  within!(time_based_df, quote
          Date = $(bar)
