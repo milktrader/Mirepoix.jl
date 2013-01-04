e54f92ae4f0794f156e1a125d18ec92303c40e8f
milktrader <milktrader@gmail.com>
2013-01-04 17:25:47 -0500

    add_sma is now a working copy

--- add_sma.jl
+++ add_sma.jl
@@ -2,25 +2,25 @@
 # an additional column whose value is a simple moving average
 
 
-function add_sma(df::DataFrame, col::ASCIIString, n::Int64)
+function uoo(df::DataFrame, n::Int64, col::ASCIIString)
 
-  new_col = strcat("ma.", string(n))
-
-  function hoo(x,n)
+  function sma(x,n)
     foo = [sum(x[i:i+(n-1)])/n for i=1:length(x)-(n-1)]
     bar = DataArray([zeros(n-1) ; foo])
     for i in 1:length(bar)
-      if bar[i] == 0
-        bar[i] = NA
-      end
+    if bar[i] == 0
+    bar[i] = NA
+    end
     end
     bar
   end
 
+  new_col = strcat("ma.", string(n))
 
   within!(df, quote
-        new_col = $hoo(vector($df["Close"]), n)
+    $new_col  = $sma(vector($df[$col]), $n)
         end);
+
   df
 end
 
