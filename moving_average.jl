ac5c432235d71fbea9158ec735fccc428ab75dbe
milktrader <milktrader@gmail.com>
2013-01-07 12:31:30 -0500

    removing lag and moving average funcitons now implemented in Thyme

--- moving_average.jl
+++
@@ -1,3 +0,0 @@
-function moving_average(x,n)
-  [sum(x[i:i+(n-1)])/n for i=1:length(x)-(n-1)]
-end
--- sma.jl
+++
@@ -1,5 +0,0 @@
-function sma(x,n)
-  foo = [mean(x[i:i+(n-1)]) for i=1:length(x)-(n-1)]
-  bar = [nas(DataVector[float(n)], n-1) ; foo]
-  bar
-end
